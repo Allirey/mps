85789ece78c11ba02dd4fcee2dd155690119961f
Allirey <slavik.1121@gmail.com>
2020-12-24 21:13:11 +0200

    updated 404 page

--- frontend/src/errors/error404.js
+++ frontend/src/errors/error404.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import {Grid, makeStyles} from "@material-ui/core";
 
-export default class extends React.Component {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+        },
+    },
+}));
 
-    render() {
-        return <h1>PAGE NOT FOUND</h1>
-    }
+const Iframe = () => (
+    <iframe src="https://lichess.org/training/frame?theme=blue&bg=light"
+            style={{width: "380px", height: "421px"}}
+            frameBorder="0">
+    </iframe>
+)
+
+export default function (props) {
+    const classes = useStyles();
+
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{minHeight: '85vh'}}
+        >
+            <Grid item md={6}>
+                <p><h1>404</h1> The requested URL <b>{`${props.location.pathname}`}</b> was not found on this
+                    server.<br/> Can you solve this one?
+                </p>
+            </Grid>
+            <Grid item xs={3}>
+                <Iframe/>
+            </Grid>
+        </Grid>
+    );
 }
+
+
