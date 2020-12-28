166025b1f7c36e7b1d6e177297dc283c20c399b5
Allirey <slavik.1121@gmail.com>
2020-12-28 18:50:40 +0200

    added forgot password page

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -4,23 +4,25 @@ import {
     Switch,
     Route
 } from "react-router-dom";
-import P404 from "../errors/error404"
-import withStore from '../hocs/withStore'
-import Header from '../components/header'
+import P404 from "../errors/error404";
+import withStore from '../hocs/withStore';
+import Header from '../components/header';
+import PrivateRoute from "../components/PrivateRoute";
+import Spinner from "../components/spinner";
 import HomePage from "../pages/HomePage";
 import About from "../pages/About";
 import ChessAnalysis from "../pages/chess/ChessAnalysis";
 import Quizy from "../pages/Quizy";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import PrivateRoute from "../components/PrivateRoute";
-import Spinner from "../components/spinner";
+import PasswordReset from "../pages/PasswordReset";
 
 function App(props) {
     const [appLoading, setAppLoading] = useState(true)
 
     useEffect(() => {
-        props.stores.authStore.refresh().then(()=>setAppLoading(false)).catch((e)=>{})
+        props.stores.authStore.refresh().then(() => setAppLoading(false)).catch((e) => {
+        })
     }, [])
 
     if (appLoading) return <Spinner/>
@@ -35,6 +37,7 @@ function App(props) {
                     <Route path="/chess/analysis" exact={true} component={ChessAnalysis}/>
                     <Route path="/login" exact={true} component={Login}/>
                     <Route path="/signup" exact={true} component={Register}/>
+                    <Route path="/forgot-password" exact={true} component={PasswordReset}/>
                     <Route path="/about" exact={true} component={About}/>
                     <Route path="**" exact={true} component={P404}/>
                 </Switch>
---
+++ frontend/src/pages/PasswordReset.js
@@ -0,0 +1,145 @@
+import React, {useEffect, useState} from 'react';
+import {Link, Redirect} from "react-router-dom";
+import {Button, CssBaseline, TextField, Grid, Box, Typography, makeStyles, Container} from '@material-ui/core';
+import withStore from '../hocs/withStore';
+import forgotImg from "./imgs/auth.png";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" to="/">
+                glitcher.org
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(9),
+    },
+    submit: {
+        margin: theme.spacing(8, 0, 2),
+        backgroundColor: "#3b535f", //   #b0bec5
+        "&:hover": {backgroundColor: "#b0bec5",}
+    },
+    logo: {
+        width: "21em",
+        "& img": {width: "100%", height: "100%"}
+    }
+}));
+
+function ResetPassword(props) {
+    const classes = useStyles();
+
+    const [emailErrTxt, setEmailErrTxt] = useState('');
+    const {inProgress, reset, setEmail} = props.stores.authStore;
+    const {email} = props.stores.authStore.values
+
+    useEffect(() => {
+        return () => reset()
+    }, [])
+
+    const setError = (errors) => {
+        errors[email] && setEmailErrTxt(errors[email])
+    }
+
+    const isEmailValid = () => {
+        const supportedEmailList = [
+            'gmail.com', 'mail.ru', 'protonmail.com',
+            'yahoo.com', 'googlemail.com', 'zoho.com',
+            'hotmail.com', 'live.com', 'msn.com', 'aol.com', 'yandex.ru',
+        ]
+
+        let isValid = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email);
+        setEmailErrTxt(isValid ? 'reset password functionality under development, sorry for inconveniences' :
+            email.length ? 'Please enter a valid email address.' : 'This field is required');
+
+        if (!isValid) return isValid;
+
+        if (!supportedEmailList.includes(email.split('@')[1])) {
+            setEmailErrTxt(`Sorry, ${email.split('@')[1]} not supported`)
+            return false
+        }
+
+        return isValid;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!isEmailValid()) return
+
+        // users.register().then(() => props.history.replace("/")).catch(setError);
+    }
+
+    if (props.stores.authStore.isAuthenticated) return <Redirect to={"/"}/>
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    Reset Password
+                </Typography>
+                <div className={classes.logo} >
+                    <img src={forgotImg} alt={''}/>
+                </div>
+                <form
+                    className={classes.form}
+                    noValidate
+                    onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                variant="outlined"
+                                // required
+                                fullWidth
+                                id="email"
+                                label="E-mail address"
+                                name="email"
+                                autoComplete="off"
+                                error={!!emailErrTxt}
+                                helperText={emailErrTxt}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onSubmit={handleSubmit}
+                        disabled={inProgress}
+                    >
+                        Reset My Password
+                    </Button>
+                    <Grid container justify="flex-start">
+                        <Grid item>
+                            <Link to="/login" variant="body2">
+                                Back to Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={5}>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
+}
+
+export default withStore(ResetPassword);
