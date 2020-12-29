f9c3cc2084927d8b1959113649a7f7e971efdf50
Allirey <slavik.1121@gmail.com>
2020-12-30 00:12:42 +0200

    updated user model: active default false, username case sensitive fix

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
+
+
+class CaseInsensitiveUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        return self.get(**{self.model.USERNAME_FIELD + '__iexact': username})
 
 
 class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
+    is_active = models.BooleanField(_('active'), default=False, help_text=_(
+        'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'
+        ), )
     last_visit = models.DateTimeField(auto_now_add=True)
+
+    objects = CaseInsensitiveUserManager()
