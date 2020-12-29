8da6e4be885ae33172ee6801e6a32c32c80f669c
Allirey <slavik.1121@gmail.com>
2020-12-29 23:52:11 +0200

    added simple username, email, password validation

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as TokenObtainSerializerNative
+from django.utils.translation import ugettext_lazy as _
 
 from .models import User
 
@@ -7,7 +8,7 @@ from .models import User
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('id', 'username', 'password', 'email')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -19,6 +20,34 @@ class UserCreateSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+    def validate_password(self, password):
+        # todo complete password validation
+        if len(password) < 6:
+            raise serializers.ValidationError(_(u'Password should be at least 6 characters'))
+
+        return password
+
+    def validate_username(self, username):
+        if User.objects.filter(username__iexact=username).exists():
+            raise serializers.ValidationError(_(u'Username already in use'))
+
+        elif len(username) < 3:
+            raise serializers.ValidationError(_(u'Username too short'))
+
+        return username
+
+    def validate_email(self, email):
+        email = email.lower()
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError(_(u'Email already in use'))
+
+        # todo supported email should be in settings
+        if not (email.split('@')[1] in ['gmail.com', 'mail.ru', 'protonmail.com', 'yahoo.com', 'googlemail.com',
+                                        'zoho.com', 'hotmail.com', 'live.com', 'msn.com', 'aol.com', 'yandex.ru', ]):
+            raise serializers.ValidationError(_(u'{} not supported'.format(email.split('@')[1])))
+
+        return email
 
 
 class TokenObtainPairSerializer(TokenObtainSerializerNative):
