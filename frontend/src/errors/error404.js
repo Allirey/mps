aa2d7de893f7336eac2b4a654f1756725f764e74
Allirey <slavik.1121@gmail.com>
2020-12-31 23:11:27 +0200

    fix for mobile

--- frontend/src/errors/error404.js
+++ frontend/src/errors/error404.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Grid, makeStyles, Container} from "@material-ui/core";
+import {Grid, makeStyles, Container, Box} from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
         '& > * + *': {
             marginLeft: theme.spacing(2),
         },
     },
+
 }));
 
 export default function (props) {
     const classes = useStyles();
-
+//todo simplify return iframe size for mobile and desktop
     return (
         <Container>
             <Grid
@@ -21,20 +22,28 @@ export default function (props) {
                 direction="column"
                 alignItems="center"
                 justify="center"
-                style={{minHeight: '85vh'}}
             >
                 <Grid item md={6}>
                     <p><h1>404</h1> The requested URL <b>{`${props.location.pathname}`}</b> was not found on this
                         server.<br/> Can you solve this one?
                     </p>
+
                 </Grid>
-                <Grid item>
+                <Box display={{xs: "none", md: "block"}}>
                     <iframe src="https://lichess.org/training/frame?theme=blue&bg=light"
-                            style={{width: "50vh", height: "100vw"}}
+                            style={{width: "50vh", height: "35vw"}}
                             frameBorder="0">
                     </iframe>
-                </Grid>
-            </Grid></Container>
+                </Box>
+
+                <Box display={{xs: "block", md: "none"}}>
+                    <iframe src="https://lichess.org/training/frame?theme=blue&bg=light"
+                            style={{width: "40vh", height: "85vw"}}
+                            frameBorder="0">
+                    </iframe>
+                </Box>
+            </Grid>
+        </Container>
     );
 }
 
