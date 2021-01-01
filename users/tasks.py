e873c3e19d525256d16cc44a75ef444525838aa6
Allirey <slavik.1121@gmail.com>
2021-01-01 21:15:27 +0200

    fix: added set header host to nginx config, activate link simplified

--- users/tasks.py
+++ users/tasks.py
@@ -18,11 +18,8 @@ def send_verification_email(user_id, scheme, domain, email_template=None):
             'token': account_activation_token.make_token(user)
         }
         # todo find smart solution to store url in settings or .env file to frontend activate page
-        if settings.DEBUG:
-            activation_link = f'{scheme}://{domain.split(":")[0]}{":3000"}/' \
+        activation_link = f'{scheme}://{domain.split(":")[0]}{":3000" if settings.DEBUG else ""}/' \
                           f'accounts/confirm-email/{kwargs["uidb64"]}/{kwargs["token"]}/'
-        else:
-            activation_link = f'https://glitcher.org/accounts/confirm-email/{kwargs["uidb64"]}/{kwargs["token"]}/'
 
         subject = 'Activate account'
 
