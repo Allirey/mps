2ad1962af1a4fe3f1832c80bcb46ed2b49827c04
Allirey <slavik.1121@gmail.com>
2021-01-02 02:35:51 +0200

    added img to Home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,11 +2,15 @@ import React, {useEffect} from "react";
 import {Container, makeStyles} from "@material-ui/core";
 import withStore from '../hocs/withStore';
 import SnackBar from "../components/snackbar";
-
+import logo from "./undraw_development_ouy3.svg";
 
 const useStyles = makeStyles(theme => ({
     root: {
         marginTop: theme.spacing(10),
+    },
+    logo: {
+        width: "21em",
+        "& img": {width: "100%", height: "100%"}
     },
 }))
 
@@ -20,15 +24,20 @@ function HomePage(props) {
     }
 
     useEffect(() => {
-            return ()=>props.stores.authStore.setShowSuccessRegister(false);
-        }, [])
+        return () => props.stores.authStore.setShowSuccessRegister(false);
+    }, [])
 
     return (
         <Container className={classes.root}>
             <SnackBar open={open}
                       text={'Activation link has been sent to your email. Please check your mailbox.'}
                       onClose={handleClose}/>
+
             <h1>Development in progress....</h1>
+
+            <div className={classes.logo}>
+                <img src={logo} alt={''}/>
+            </div>
         </Container>
     )
 }
