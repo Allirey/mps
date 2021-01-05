0f73257b01c51b0a1c230b2d5ce5aa9bd8270bb2
Allirey <slavik.1121@gmail.com>
2021-01-05 19:08:25 +0200

    added todos on home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,21 @@
 import React, {useEffect} from "react";
-import {Container, makeStyles} from "@material-ui/core";
+import {Container, makeStyles, List, ListItem, Typography} from "@material-ui/core";
 import withStore from '../hocs/withStore';
 import SnackBar from "../components/snackbar";
 import logo from "./undraw_development_ouy3.svg";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        marginTop: theme.spacing(10),
+        marginTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
     },
     logo: {
         width: "21em",
         "& img": {width: "100%", height: "100%"}
     },
+    todos:{
+        marginTop: theme.spacing(2)
+    }
 }))
 
 function HomePage(props) {
@@ -33,11 +37,26 @@ function HomePage(props) {
                       text={'Activation link has been sent to your email. Please check your mailbox.'}
                       onClose={handleClose}/>
 
-            <h1>Development in progress....</h1>
+            <Typography variant={"h3"}>Development in progress....</Typography>
 
             <div className={classes.logo}>
                 <img src={logo} alt={''}/>
             </div>
+            <Typography variant={"h4"} className={classes.todos}>Current todos:</Typography>
+            <List>
+                <ListItem>password change</ListItem>
+                <ListItem>password recover</ListItem>
+                <ListItem>edit user info in settings</ListItem>
+                <ListItem>quizy rewrite using db</ListItem>
+                <ListItem>redesign chess db</ListItem>
+                <ListItem>blog</ListItem>
+                <ListItem>admin/moderator page</ListItem>
+                <ListItem>live chat</ListItem>
+                <ListItem>chess tactics</ListItem>
+                <ListItem>chess theory</ListItem>
+            </List>
+
+
         </Container>
     )
 }
