f251fdce9b35531146c768bc9b71eeef7deef86c
Allirey <slavik.1121@gmail.com>
2021-01-08 15:55:17 +0200

    added notification for privateRoute

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -5,6 +5,7 @@ import withStore from '../hocs/withStore';
 function PrivateRoute(props) {
     const {stores, ...restProps} = props;
     if (stores.authStore.isAuthenticated) return <Route {...restProps} />;
+    props.stores.notifications.notify("Please, sign in to see this page.",3)
     return <Redirect to={{pathname: '/login', state: {from: props.location}}}/>;
 }
 
