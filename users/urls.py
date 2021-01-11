b98ceb3ad73ba7280352d071c870167f963045f5
Allirey <slavik.1121@gmail.com>
2021-01-11 17:50:53 +0200

    bugfix: incorrect order in urls patterns

--- users/urls.py
+++ users/urls.py
@@ -18,12 +18,12 @@ urlpatterns = [
     path('users/create/', views.UserCreateView.as_view(), name='sign-up'),
     path('users/activate/<uidb64>/<token>/', views.ActivateView.as_view(), name='activate'),
 
-    path('users/<str:username>/', public_user_info, name='public-user'),
-
     path('users/forgot-password/', views.SendPasswordResetLinkView.as_view(), name='forgot-password-request'),
     path('users/forgot-password/<uidb64>/<token>/', views.PasswordResetChangeView.as_view(),
          name='forgot-password-verify'),
     path('users/change-password/', views.ChangePasswordView.as_view(), name='change-password'),
+
+    path('users/<str:username>/', public_user_info, name='public-user'),
 
     path('token/obtain/', views.TokenObtainPairView.as_view(), name='token-create'),
     path('token/refresh/', views.TokenRefreshView.as_view(), name='token-refresh'),
