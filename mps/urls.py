da505e512ec625036b7d2fe86d577383652d372d
Allirey <slavik.1121@gmail.com>
2021-01-13 22:24:53 +0200

    added blog api

--- mps/urls.py
+++ mps/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/', include('chess_db.urls')),
     path('api/', include('users.urls')),
-    # path('', include('blog.urls')),
+    path('api/', include('blog.urls')),
 ]
