3e9cb098718deaf5e00fc2b5355fbba954ae0ac1
Allirey <slavik.1121@gmail.com>
2021-01-19 23:44:18 +0200

    added user move support, almost

--- frontend/src/pages/chess/ChessAnalysis.js
+++ frontend/src/pages/chess/ChessAnalysis.js
@@ -1,137 +1,157 @@
-import React from "react";
-import ChessBoard from "../../components/chess/analysis/ChessBoard";
+import React, {useState, useEffect} from "react";
 import GamesTable from "../../components/chess/analysis/GamesTable";
 import GamesSearch from "../../components/chess/analysis/GamesSearch";
 import MovesTree from "../../components/chess/analysis/MovesTree";
 import Notation from "../../components/chess/analysis/Notation";
 import NavButtons from "../../components/chess/analysis/NavButtons";
 
-import {Grid, Box, makeStyles} from "@material-ui/core";
+import ChessBoard from 'react-chessground'
+// import "react-chessground/dist/assets/chessground.css"
+import "react-chessground/dist/styles/chessground.css"
+// import "react-chessground/dist/assets/theme.css"
+
+import {Grid, Box, makeStyles, useTheme, useMediaQuery} from "@material-ui/core";
 import withStore from "../../hocs/withStore";
 import StyledTabs from "../../components/StyledTabs";
+import {Helmet} from "react-helmet";
 
-class ChessAnalysis extends React.Component {
-    componentDidMount() {
-        // this.props.stores.chessNotation.initMainLineNodes('');
-        // this.props.stores.chessNotation.resetNode()
-    }
+const useStyles = makeStyles(theme => ({
+   root: {},
 
-    componentWillUnmount() {
-        this.props.stores.chessNotation.chessGame.reset();
-        this.props.stores.chessNotation.resetNode()
-    }
+   chessField: {
+        }
 
-    render() {
-        let chess = this.props.stores.chessOpeningExplorer;
-        let notation = this.props.stores.chessNotation;
+}))
 
-        return (
-            <Grid container justify={"space-evenly"}>
-                <Grid item display={{xs: "none", lg: "block"}} lg={4} component={Box}>
-                    <GamesSearch
-                        name={chess.searchData.name}
-                        color={chess.searchData.color}
-                        onSubmit={chess.searchGames}
-                        onChangeColor={e => chess.setColor(e.target.value)}
-                        onChangeName={e => chess.setName(e.target.value)}
-                        onKeyPressed={e => e.keyCode === 13 ? chess.searchGames() : []}
+const ChessAnalysis = (props) => {
+   const classes = useStyles();
+   const theme = useTheme();
+   const matchesMD = useMediaQuery(theme.breakpoints.up('md'));
+   const matchesSM = useMediaQuery(theme.breakpoints.up('sm'));
+
+   const {chessNotation: notation, chessOpeningExplorer: chess} = props.stores
+
+   useEffect(() => {
+      return () => {
+         notation.chessGame.reset()
+         notation.resetNode()
+      }
+   }, [])
+
+   return (
+     <>
+        <Helmet
+        title={"Ukrainian chess database"}
+        />
+     <Grid container justify={"space-evenly"}>
+        <Grid item display={{xs: "none", lg: "block"}} lg={4} component={Box}>
+           <GamesSearch
+             name={chess.searchData.name}
+             color={chess.searchData.color}
+             onSubmit={chess.searchGames}
+             onChangeColor={e => chess.setColor(e.target.value)}
+             onChangeName={e => chess.setName(e.target.value)}
+             onKeyPressed={e => e.keyCode === 13 ? chess.searchGames() : []}
+           />
+           <GamesTable
+             games={chess.currentGames}
+             onSelectGame={chess.getGameByUrl}
+           />
+        </Grid>
+
+        <Grid item container xs={12} sm={12} md={7} lg={8} justify={"center"} >
+           <Grid item lg={8} container direction={"column"}>
+
+              <Grid item>
+                 <div className={classes.chessField}
+                      onWheel={e => e.deltaY < 0 ? notation.toPrev() : notation.toNext()}>
+                    <ChessBoard
+                      width={matchesSM ? "512px" : "90vmin"}
+                      height={matchesSM ? "512px" : "90vmin"}
+                      orientation={notation.boardOrientation}
+                      viewOnly={false}
+                      turnColor={notation.turnColor()}
+                      movable={notation.calcMovable()}
+                      // lastMove={notation.lastMove()}
+                      fen={notation.currentFen}
+                      // check={"false"}
+                      style={{margin: "auto"}}
+                      coordinates={false}
+                      onMove={notation.onMove}
                     />
-                    <GamesTable
-                        games={chess.currentGames}
-                        onSelectGame={chess.getGameByUrl}
+                 </div>
+              </Grid>
+
+              <Box display={{sm: "block", lg: "none"}}>
+                 <Grid item container justify={"center"}>
+                    <NavButtons
+                      toFirst={notation.toFirst}
+                      toLast={notation.toLast}
+                      toNext={notation.toNext}
+                      toPrev={notation.toPrev}
+                      onFlip={notation.flipBoard}
                     />
-                </Grid>
+                 </Grid>
+              </Box>
+           </Grid>
 
-                <Grid item container xs={12} sm={12} md={7} lg={8} justify={"center"} >
-                    <Grid item lg={8} container direction={"column"}>
+           <Grid item component={Box} display={{xs: "none", lg: "block"}} lg={4}>
+              <Notation
+                notation={notation.mainLineNodes}
+                currentNode={notation.node}
+                jumpTo={notation.jumpToMove}
+              />
+              <br/>
+              <MovesTree explorerData={chess.currentMoves}/>
+              <Grid component={Box} container justify={"center"} display={{sm: "none", lg: "block"}}>
+                 <NavButtons
+                   toFirst={notation.toFirst}
+                   toLast={notation.toLast}
+                   toNext={notation.toNext}
+                   toPrev={notation.toPrev}
+                   onFlip={notation.flipBoard}
+                 />
+              </Grid>
+           </Grid>
 
-                           <Grid item> <div onWheel={e => e.deltaY < 0 ? notation.toPrev() : notation.toNext()}>
-                                <ChessBoard
-                                    width={window.innerWidth > 950 ? "512px" : "90vmin"}
-                                    height={window.innerWidth > 950 ? "512px" : "90vmin"}
-                                    orientation={notation.boardOrientation}
-                                    viewOnly={false}
-                                    // turnColor={chess.turnColor()}
-                                    // movable={chess.calcMovable()}
-                                    lastMove={notation.lastMove}
-                                    fen={notation.fen}
-                                    // onMove={chess.onMove}
-                                    check={"false"}
-                                    style={{margin: "auto"}}
-                                />
-                            </div></Grid>
+        </Grid>
 
-                        <Box display={{sm: "block", lg: "none"}}>
-                            <Grid item container justify={"center"}>
-                                <NavButtons
-                                    toFirst={notation.toFirst}
-                                    toLast={notation.toLast}
-                                    toNext={notation.toNext}
-                                    toPrev={notation.toPrev}
-                                    onFlip={notation.flipBoard}
-                                />
-                            </Grid>
-                        </Box>
-                    </Grid>
-
-                    <Grid item component={Box} display={{xs: "none", lg: "block"}} lg={4}>
-                        <Notation
-                            notation={notation.mainLineNodes}
-                            currentNode={notation.node}
-                            jumpTo={notation.jumpToMove}
-                        />
-                        <br/>
-                        <MovesTree explorerData={chess.currentMoves}/>
-                        <Grid component={Box} container justify={"center"} display={{sm: "none", lg: "block"}}>
-                            <NavButtons
-                                toFirst={notation.toFirst}
-                                toLast={notation.toLast}
-                                toNext={notation.toNext}
-                                toPrev={notation.toPrev}
-                                onFlip={notation.flipBoard}
-                            />
-                        </Grid>
-                    </Grid>
-
-                </Grid>
-
-                <Grid item component={Box} display={{xs: "block", lg: "none"}} xs sm md={5}>
-                    <StyledTabs variant={"fullWidth"}
-                        tabs={{
-                            "Notation":
+        <Grid item component={Box} display={{xs: "block", lg: "none"}} xs sm md={5}>
+           <StyledTabs variant={"fullWidth"}
+                       tabs={{
+                          "Notation":
                             <>
-                                <Notation
-                                    notation={notation.mainLineNodes}
-                                    currentNode={notation.node}
-                                    jumpTo={notation.jumpToMove}
-                                />
-                                </>
-                            ,
-                            "Games":
-                                <>
-                                <GamesSearch
-                                    name={chess.searchData.name}
-                                    color={chess.searchData.color}
-                                    onSubmit={chess.searchGames}
-                                    onChangeColor={e => chess.setColor(e.target.value)}
-                                    onChangeName={e => chess.setName(e.target.value)}
-                                    onKeyPressed={e => e.keyCode === 13 ? chess.searchGames() : []}
-                                />
-                                <GamesTable
-                                    games={chess.currentGames}
-                                    onSelectGame={chess.getGameByUrl}
-                                />
-                            </>,
-                            "Book":
-                            <>
-                            <MovesTree explorerData={chess.currentMoves}/>
+                               <Notation
+                                 notation={notation.mainLineNodes}
+                                 currentNode={notation.node}
+                                 jumpTo={notation.jumpToMove}
+                               />
                             </>
-                        }}
-                    />
-                </Grid>
-            </Grid>
-        )
-    }
+                          ,
+                          "Games":
+                            <>
+                               <GamesSearch
+                                 name={chess.searchData.name}
+                                 color={chess.searchData.color}
+                                 onSubmit={chess.searchGames}
+                                 onChangeColor={e => chess.setColor(e.target.value)}
+                                 onChangeName={e => chess.setName(e.target.value)}
+                                 onKeyPressed={e => e.keyCode === 13 ? chess.searchGames() : []}
+                               />
+                               <GamesTable
+                                 games={chess.currentGames}
+                                 onSelectGame={chess.getGameByUrl}
+                               />
+                            </>,
+                          "Book":
+                            <>
+                               <MovesTree explorerData={chess.currentMoves}/>
+                            </>
+                       }}
+           />
+        </Grid>
+     </Grid></>
+   )
 }
 
 export default withStore(ChessAnalysis)
--- frontend/src/store/notationStore.js
+++ frontend/src/store/notationStore.js
@@ -2,123 +2,172 @@ import {observable, computed, action, decorate} from 'mobx';
 import Chess from 'chess.js'
 
 class NotationStore {
-    constructor(rootStore) {
-        this.rootStore = rootStore;
-    }
+   constructor(rootStore) {
+      this.rootStore = rootStore;
+   }
 
-    chessGame = new Chess();
+   chessGame = new Chess();
 
-    node = {
-        fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-        ply: 0,
-        san: '',
-        uci: {from: null, to: null},
-    };
+   node = {
+      fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+      ply: 0,
+      san: '',
+      uci: {from: null, to: null},
+   };
 
-    mainLineNodes = [];
-    boardOrientation = "white";
+   mainLineNodes = [];
+   boardOrientation = "white";
 
-    initMainLineNodes = (pgn) => {
-        this.resetNode();
+   initMainLineNodes = (pgn) => {
+      this.resetNode();
 
-        if (pgn === null) return
+      if (pgn === null) return
 
-        this.chessGame.load_pgn(pgn, { sloppy: true });
-        let tmp = this.chessGame.history();
-        this.chessGame = new Chess();
+      this.chessGame.load_pgn(pgn, {sloppy: true});
+      let tmp = this.chessGame.history();
+      this.chessGame = new Chess();
 
-        this.mainLineNodes = tmp.map((srcMove, i) => {
-            let move = this.chessGame.move(srcMove, { sloppy: true });
-            return {
-                fen: this.chessGame.fen(),
-                san: move.san,
-                ply: i,
-                uci: {from: move.from, to: move.to}
-            }
-        });
-    };
+      this.mainLineNodes = tmp.map((srcMove, i) => {
+         let move = this.chessGame.move(srcMove, {sloppy: true});
+         return {
+            fen: this.chessGame.fen(),
+            san: move.san,
+            ply: i,
+            uci: {from: move.from, to: move.to}
+         }
+      });
+   };
 
-    flipBoard = () => this.boardOrientation = this.boardOrientation === "white" ? "black" : "white";
+   flipBoard = () => this.boardOrientation = this.boardOrientation === "white" ? "black" : "white";
 
-    jumpToNode(index) {
-        let currentIndex = this.mainLineNodes.findIndex((obj) => obj === this.node, this.node);
-        let newIndex = currentIndex + index;
-        if (newIndex < 0 || this.mainLineNodes.length === 0) {
-            this.node = {
-                fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-                ply: 0,
-                san: '',
-                uci: {from: null, to: null},
-            };
-        } else {
-            this.node = this.mainLineNodes[Math.min(newIndex, this.mainLineNodes.length - 1)]
-        }
-        this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
-        this.rootStore.chessOpeningExplorer.searchGames();
-    }
-
-    get lastMove() {
-        return typeof this.node === "undefined" || this.node.uci.from === null ? [] : [this.node.uci.from, this.node.uci.to];
-    }
-
-    get fen() {
-        return typeof this.node === "undefined" ?
-            'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' : this.node.fen;
-    }
-
-    jumpToMove = (index) => {
-        this.node = this.mainLineNodes[index]
-        this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
-        this.rootStore.chessOpeningExplorer.searchGames();
-    };
-
-    toNext = () => {
-        this.jumpToNode(1)
-    };
-
-    toPrev = () => {
-        this.jumpToNode(-1)
-    };
-
-    toFirst = () => {
-        this.jumpToNode(-1000)
-    };
-
-    toLast = () => {
-        this.jumpToNode(this.mainLineNodes.length - this.mainLineNodes.findIndex((obj) => obj === this.node, this.node) - 1)
-    };
-
-    resetNode = () => {
-        this.mainLineNodes = [];
-        this.chessGame.reset();
-        this.node = {
+   jumpToNode(index) {
+      let currentIndex = this.mainLineNodes.findIndex((obj) => obj === this.node, this.node);
+      let newIndex = currentIndex + index;
+      if (newIndex < 0 || this.mainLineNodes.length === 0) {
+         this.node = {
             fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
             ply: 0,
             san: '',
             uci: {from: null, to: null},
-        };
-        this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
-        this.rootStore.chessOpeningExplorer.searchGames();
-    }
+         };
+      } else {
+         this.node = this.mainLineNodes[Math.min(newIndex, this.mainLineNodes.length - 1)]
+      }
+      this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
+      this.rootStore.chessOpeningExplorer.searchGames();
+   }
+
+   get lastMove() {
+      return typeof this.node === "undefined" || this.node.uci.from === null ? [] : [this.node.uci.from, this.node.uci.to];
+   }
+
+   get fen() {
+      return typeof this.node === "undefined" ?
+        'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' : this.node.fen;
+   }
+
+   jumpToMove = (index) => {
+      this.node = this.mainLineNodes[index]
+      this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
+      this.rootStore.chessOpeningExplorer.searchGames();
+   };
+
+   toNext = () => {
+      this.jumpToNode(1)
+   };
+
+   toPrev = () => {
+      this.jumpToNode(-1)
+   };
+
+   toFirst = () => {
+      this.jumpToNode(-1000)
+   };
+
+   toLast = () => {
+      this.jumpToNode(this.mainLineNodes.length - this.mainLineNodes.findIndex((obj) => obj === this.node, this.node) - 1)
+   };
+
+   resetNode = () => {
+      this.mainLineNodes = [];
+      this.chessGame.reset();
+      this.node = {
+         fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+         ply: 0,
+         san: '',
+         uci: {from: null, to: null},
+      };
+      this.rootStore.chessOpeningExplorer.searchData.fen = this.node.fen
+      this.rootStore.chessOpeningExplorer.searchGames();
+   }
+
+   nodes = []
+   currentNode = {
+      fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+      san: ''
+   }
+   currentFen = ''
+
+   onMove = (from, to) => {
+      console.log(from, to)
+
+      let move = this.chessGame.move({from, to})
+      if (move) {
+         this.currentNode.san = this.chessGame.history()[this.chessGame.history().length - 1]
+         this.nodes.push(this.currentNode)
+         this.currentNode = {fen: this.chessGame.fen(), san:''}
+
+         this.currentFen = this.chessGame.fen()
+      }
+      console.log(this.chessGame.history())
+
+   }
+
+   calcMovable() {
+      const dests = new Map()
+      this.chessGame.SQUARES.forEach(s => {
+         const ms = this.chessGame.moves({square: s, verbose: true})
+         if (ms.length) dests.set(s, ms.map(m => m.to))
+      })
+      return {
+         free: false,
+         dests,
+         color: this.turnColor()
+      }
+   }
+
+   turnColor() {
+      return this.chessGame.turn() === "w" ? "white" : "black"
+   }
 }
 
 decorate(NotationStore, {
-        node: observable,
-        mainLineNodes: observable,
-        boardOrientation: observable,
+     node: observable,
+     mainLineNodes: observable,
+     boardOrientation: observable,
 
-        initMainLineNodes: action,
-        flipBoard: action,
-        jumpToMove: action,
-        toNext: action,
-        toPrev: action,
-        toFirst: action,
-        toLast: action,
-        resetNode: action,
+     // chessGame: observable,
 
-        lastMove: computed,
-        fen: computed,
-    }
+
+
+     nodes: observable,
+     currentNode: observable,
+     currentFen: observable,
+
+
+     initMainLineNodes: action,
+     flipBoard: action,
+     jumpToMove: action,
+     toNext: action,
+     toPrev: action,
+     toFirst: action,
+     toLast: action,
+     resetNode: action,
+     onMove: action,
+
+     lastMove: computed,
+     fen: computed,
+  }
 );
 
 export default NotationStore;
