618c09f613fc055919933aaf2490cc93100eb387
Allirey <slavik.1121@gmail.com>
2021-01-20 12:19:38 +0200

    chunk bundles

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import P404 from "../errors/error404";
 import withStore from '../hocs/withStore';
 import Header from '../components/header';
 import PrivateRoute from "../components/PrivateRoute";
-import HomePage from "../pages/HomePage";
-import About from "../pages/About";
-import ChessAnalysis from "../pages/chess/ChessAnalysis";
-import Login from "../pages/Auth/Login";
-import Register from "../pages/Auth/Register";
-import PasswordResetRequest from "../pages/Auth/PasswordResetRequest";
-import PasswordResetChange from "../pages/Auth/PasswordResetChange";
-import AccountActivation from "../pages/Auth/AccountActivation";
-import ChangePassword from "../pages/Auth/ChangePassword";
-import UserProfile from "../pages/UserProfile";
-import EditProfile from "../pages/EditProfile";
 import SnackBar from "../components/snackbar";
-import ArticleDetail from "../pages/Blog/ArticleDetail";
-import Articles from '../pages/Blog/Articles';
 import {Helmet} from "react-helmet";
+
+const Articles = lazy(() => import("../pages/Blog/Articles"));
+const ArticleDetail = lazy(() => import("../pages/Blog/ArticleDetail"));
+const Register = lazy(() => import("../pages/Auth/Register"));
+const Login = lazy(() => import("../pages/Auth/Login"));
+const About = lazy(() => import("../pages/About"));
+const UserProfile = lazy(() => import("../pages/UserProfile"));
+const EditProfile = lazy(() => import("../pages/EditProfile"));
+const ChangePassword = lazy(() => import("../pages/Auth/ChangePassword"));
+const AccountActivation = lazy(() => import("../pages/Auth/AccountActivation"));
+const PasswordResetRequest = lazy(() => import("../pages/Auth/PasswordResetRequest"));
+const PasswordResetChange = lazy(() => import("../pages/Auth/PasswordResetChange"));
+const ChessAnalysis = lazy(() => import("../pages/chess/ChessAnalysis"));
+const Error404 = lazy(() => import("../errors/error404"));
+const HomePage = lazy(() => import("../pages/HomePage"));
 
 function App(props) {
    const [appLoading, setAppLoading] = useState(true)
-
    const {notifications} = props.stores
 
    useEffect(() => {
@@ -44,29 +44,30 @@ function App(props) {
           severity={notifications.severity}
         />
         <div>
-           <Switch>
-              <Route path="/" exact={true} component={HomePage}/>
-              <Route path="/chess/analysis" exact={true} component={ChessAnalysis}/>
-              <Route path="/login" exact={true} component={Login}/>
-              <Route path="/signup" exact={true} component={Register}/>
-              <Route path="/blog" exact={true} component={Articles}/>
-              <Route path="/blog/page/:currentPage" exact={true} component={Articles}/>
-              <Route path="/blog/new" exact={true} component={ArticleDetail}/>
-              <Route path="/blog/:slug" exact={true} component={ArticleDetail}/>
-              <Route path="/accounts/confirm-email/:id/:token" exact={true} component={AccountActivation}/>
-              <PrivateRoute path="/accounts/password/change" exact={true} component={ChangePassword}/>
-              <Route path="/accounts/password/reset" exact={true} component={PasswordResetRequest}/>
-              <Route path="/accounts/password/reset/:id/:token" exact={true} component={PasswordResetChange}/>
-              <Route path="/about" exact={true} component={About}/>
-              <PrivateRoute path="/settings" exact={true} component={EditProfile}/>
-              <Route path="/users" exact={true} component={HomePage}/>
-              <Route path="/users/:username" exact={true} component={UserProfile}/>
-              <Route path="**" exact={true} component={P404}/>
-           </Switch>
+           <Suspense fallback={<div/>}>
+              <Switch>
+                 <Route path="/" exact={true} component={HomePage}/>
+                 <Route path="/chess/analysis" exact={true} component={ChessAnalysis}/>
+                 <Route path="/login" exact={true} component={Login}/>
+                 <Route path="/signup" exact={true} component={Register}/>
+                 <Route path="/blog" exact={true} component={Articles}/>
+                 <Route path="/blog/page/:currentPage" exact={true} component={Articles}/>
+                 <Route path="/blog/new" exact={true} component={ArticleDetail}/>
+                 <Route path="/blog/:slug" exact={true} component={ArticleDetail}/>
+                 <Route path="/accounts/confirm-email/:id/:token" exact={true} component={AccountActivation}/>
+                 <PrivateRoute path="/accounts/password/change" exact={true} component={ChangePassword}/>
+                 <Route path="/accounts/password/reset" exact={true} component={PasswordResetRequest}/>
+                 <Route path="/accounts/password/reset/:id/:token" exact={true} component={PasswordResetChange}/>
+                 <Route path="/about" exact={true} component={About}/>
+                 <PrivateRoute path="/settings" exact={true} component={EditProfile}/>
+                 <Route path="/users" exact={true} component={HomePage}/>
+                 <Route path="/users/:username" exact={true} component={UserProfile}/>
+                 <Route path="**" exact={true} component={Error404}/>
+              </Switch>
+           </Suspense>
         </div>
      </Router>
    )
-
 }
 
 export default withStore(App);
