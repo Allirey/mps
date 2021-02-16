00d0a4cc0f66b2ef5dfde35cca95bdb20e4f665d
Allirey <slavik.1121@gmail.com>
2021-02-16 20:41:39 +0200

    added opening tag api

--- chess_openings/urls.py
+++ chess_openings/urls.py
@@ -4,10 +4,10 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('openings', views.OpeningViewSet)
+router.register('openings/tags', views.TagViewSet)
 
 urlpatterns = [
     path('openings/create/', views.CreateOpeningView.as_view(), name='opening-view'),
     path('openings/chapters/<int:pk>', views.ChapterDetailView.as_view(), name='chapter-view'),
-    path('openings/tags/', views.TagViewSet.as_view(), name='tags-view')
 ]
 urlpatterns += router.urls
