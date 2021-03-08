b229a5ff850a7d8bc0ccd11e674f7740a8517c81
Allirey <slavik.1121@gmail.com>
2021-03-08 22:16:12 +0200

    refactoring

---
+++ frontend/src/components/chess/ChessBoard/index.js
@@ -0,0 +1,91 @@
+import React, {useEffect} from 'react';
+import withStore from "../../../hocs/withStore";
+import ChessGround from "react-chessground";
+import {Button, Dialog, DialogActions, Paper, useMediaQuery, useTheme} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import ChessBoardBlueTheme from "./chessBoardBlue.svg";
+import "react-chessground/dist/styles/chessground.css"
+import "./cburnett.css"
+import wQ from "./pieces/wQ.svg";
+import wN from "./pieces/wN.svg";
+import wR from "./pieces/wR.svg";
+import wB from "./pieces/wB.svg";
+import bQ from "./pieces/bQ.svg";
+import bN from "./pieces/bN.svg";
+import bR from "./pieces/bR.svg";
+import bB from "./pieces/bB.svg";
+
+const pieceImages = {
+   'white': {'q': wQ, 'n': wN, 'r': wR, 'b': wB,},
+   'black': {'q': bQ, 'n': bN, 'r': bR, 'b': bB,},
+}
+
+const useStyles = makeStyles(theme => ({
+   root: {},
+   chessField: {
+      "& .cg-wrap": {
+         backgroundImage: `url(${ChessBoardBlueTheme})`
+      },
+      "& cg-board square.last-move": {
+         backgroundColor: "lightgreen",
+         opacity: "0.41"
+      },
+      [theme.breakpoints.up('sm')]: {
+         marginRight: "24px",
+      },
+   },
+}))
+
+const ChessBoard = ({stores}) => {
+   const classes = useStyles()
+   const theme = useTheme()
+   const matchesLG = useMediaQuery(theme.breakpoints.up('lg'));
+   const matchesMD = useMediaQuery(theme.breakpoints.up('md'));
+   const matchesOnlyXS = useMediaQuery(theme.breakpoints.only('xs'));
+
+   let refEl = React.createRef();
+
+   useEffect(() => {
+      refEl && refEl.addEventListener('wheel', e => e.preventDefault(), {passive: false});
+      return () => refEl && refEl.removeEventListener('wheel', e => e.preventDefault(), {passive: false});
+   }, [refEl])
+
+   const {chessNotation: notation} = stores
+   const onBoardWheel = e => {
+      return e.deltaY < 0 ? notation.toPrev() : notation.toNext()
+   }
+
+   return <><Paper elevation={2} ref={elem => refEl = elem} className={classes.chessField}
+                   onWheel={onBoardWheel}>
+      <ChessGround
+        width={matchesLG ? "544px" : matchesOnlyXS ? "100vmin" : matchesMD ? "448px" : "384px"}
+        height={matchesLG ? "544px" : matchesOnlyXS ? "100vmin" : matchesMD ? "448px" : "384px"}
+        orientation={notation.boardOrientation}
+        viewOnly={false}
+        turnColor={notation.turnColor()}
+        movable={notation.calcMovable}
+        lastMove={notation.lastMove}
+        fen={notation.currentNode.fen}
+        check={notation.inCheck}
+        style={{margin: "auto"}}
+        coordinates={false}
+        onMove={notation.onMove}
+      />
+   </Paper>
+      <Dialog open={notation.showPieceSelectMenu} onClose={() => {
+         notation.showPieceSelectMenu = false;
+         notation.pendingMove = null;
+      }}>
+         {['q', 'n', 'r', 'b'].map(piece =>
+           <DialogActions key={piece}>
+              <Button disableRipple onClick={() => notation.promotion(piece)}>
+                 <img src={pieceImages[notation.turnColor()][piece]} alt={''}/>
+              </Button>
+           </DialogActions>
+         )}
+      </Dialog>
+
+   </>
+}
+
+export default withStore(ChessBoard)
--- frontend/src/pages/chess/ChessAnalysis.js
+++ frontend/src/pages/chess/ChessAnalysis.js
@@ -5,30 +5,12 @@ import NavButtons from "../../components/chess/analysis/NavButtons";
 import GameInfo from "../../components/chess/analysis/GameInfo";
 import ExplorerBox from "../../components/chess/analysis/ExplorerBox";
 
-import ChessBoard from 'react-chessground'
-import "react-chessground/dist/styles/chessground.css"
-import ChessBoardBlueTheme from './chessBoardBlue.svg'
-import "./cburnett.css"
-import wR from './pieces/wR.svg'
-import wQ from './pieces/wQ.svg'
-import wB from './pieces/wB.svg'
-import wN from './pieces/wN.svg'
-import bR from './pieces/bR.svg'
-import bQ from './pieces/bQ.svg'
-import bN from './pieces/bN.svg'
-import bB from './pieces/bB.svg'
+import ChessBoard from "../../components/chess/ChessBoard";
+
 import Colobki from './undraw_elements_cipa.svg'
 // import Error404 from "../../errors/error404";
 
-import {
-   Grid,
-   makeStyles,
-   useMediaQuery,
-   Dialog,
-   DialogActions,
-   Button,
-   createMuiTheme, Fade, Paper
-} from "@material-ui/core";
+import {Grid, makeStyles, useMediaQuery, createMuiTheme, Fade} from "@material-ui/core";
 import withStore from "../../hocs/withStore";
 import {Helmet} from "react-helmet";
 import {useParams} from 'react-router-dom'
@@ -37,33 +19,23 @@ import OpeningChapters from "./OpeningChapters";
 
 const DATABASES = {UKR: 'ukr', MASTERS: "masters", LICHESS: 'lichess'}
 
-const breakpointValues = {
-   xs: 0,
-   sm: 700,
-   md: 960,
-   lg: 1280,
-   xl: 1920,
-};
+const breakpointValues = {xs: 0, sm: 700, md: 960, lg: 1280, xl: 1920,};
 const theme = createMuiTheme({breakpoints: {values: breakpointValues}});
-
-const pieceImages = {
-   'white': {'q': wQ, 'n': wN, 'r': wR, 'b': wB,},
-   'black': {'q': bQ, 'n': bN, 'r': bR, 'b': bB,},
-}
 
 const useStyles = makeStyles(theme => ({
    root: {
+      display: "flex",
       touchAction: "manipulation",
    },
-   chessField: {
-      "& .cg-wrap": {
-         backgroundImage: `url(${ChessBoardBlueTheme})`
+   content: {
+      // flexGrow: 1,
+      [theme.breakpoints.up('md')]: {
+         padding: theme.spacing(3),
       },
-      "& cg-board square.last-move": {
-         backgroundColor: "lightgreen",
-         opacity: "0.41"
+      [theme.breakpoints.only('sm')]: {
+         paddingTop: theme.spacing(3),
       },
-   }
+   },
 }))
 
 const ChessAnalysis = (props) => {
@@ -76,7 +48,6 @@ const ChessAnalysis = (props) => {
    const matchesOnlyXS = useMediaQuery(theme.breakpoints.only('xs'));
    const matchesOnlySM = useMediaQuery(theme.breakpoints.only('sm'));
    const matchesOnlyMD = useMediaQuery(theme.breakpoints.only('md'));
-   let refEl = React.createRef();
 
    const [showBook, setShowBook] = useState(false);
    const [showSearch, setShowSearch] = useState(false);
@@ -87,6 +58,10 @@ const ChessAnalysis = (props) => {
    const [notFound, setNotFound] = useState(false)
 
    const {chessNotation: notation, chessOpeningExplorer: chess, openings} = props.stores
+
+   const toggleBook = () => setShowBook(!showBook)
+   const toggleSearch = () => setShowSearch(!showSearch)
+   const toggleChapters = () => setShowChapters(!showChapters)
 
    const keyHandler = e => {
       if (e.which > 36 && e.which < 41 && e.target.tagName !== "INPUT") {
@@ -109,12 +84,8 @@ const ChessAnalysis = (props) => {
       }
    }, [])
 
-   useEffect(() => {
-      if (refEl.addEventListener) refEl.addEventListener('wheel', e => e.preventDefault(), {passive: false});
-   }, [showChapters])
 
    useEffect(() => {
-      if (refEl.hasOwnProperty('addEventListener')) refEl.addEventListener('wheel', e => e.preventDefault(), {passive: false});
       if (db && game_id) {
          chess.getGame(db, game_id).catch(() => {
             setNotFound(true)
@@ -145,7 +116,7 @@ const ChessAnalysis = (props) => {
 
    }, [slug, chapter_id])
 
-   const handleSearchSubmit = (name, color) => {
+   const handleSearchSubmit = () => {
       notation.toFirst()
       setShowSearch(false)
       setShowBook(true)
@@ -172,7 +143,7 @@ const ChessAnalysis = (props) => {
    // if (id && notFound && !loading) return <Error404/>
 
    return (
-     <>
+     <div className={classes.root}>
         <Helmet
           title={"Chess analysis board"}
         >
@@ -186,164 +157,126 @@ const ChessAnalysis = (props) => {
            <meta name="twitter:site:id" content="741164490"/>
         </Helmet>
 
-        {matchesOnlyXS && showChapters ? <OpeningChapters
-            currentOpening={openings.currentOpening}
-            onClose={() => setShowChapters(false)}
-            currentChapter={openings.currentChapter}
-          />
-          :
-          <Grid container direction={"row"} className={classes.root}>
+        {slug && <StudyChapters
+          currentOpening={openings.currentOpening}
+          currentChapter={openings.currentChapter}
+          showChapters={showChapters}
+          toggleChapters={toggleChapters}
+        />}
 
-             {matchesLG && <Grid item container direction={"column"} lg>
-                {!slug && <Grid item xs style={{maxHeight: "17vh"}}>
-                   <GameInfo data={notation.gameHeaders}/>
-                </Grid>}
-                <Grid item xs sm md lg style={{height: '73vh', overflowY: "auto"}}>
-                   {!slug ? <img src={Colobki} alt={''} width={"100%"} height={"auto"}/> :
-                     <StudyChapters
-                       currentOpening={openings.currentOpening}
-                       currentChapter={openings.currentChapter}
-                     />}
-                </Grid>
-             </Grid>}
+        <Grid container direction={"row"} className={classes.content}>
 
-             <Grid item container justify={"center"}
-                   style={{padding: matchesSM ? "8px" : 0}}
-                   sm md lg>
-                <Grid item>
-                   {matchesSM && !matchesLG && <Grid item style={{maxHeight: "17vh"}}>
-                      <GameInfo data={notation.gameHeaders}/>
-                   </Grid>}
-                   <Paper elevation={2} ref={elem => refEl = elem} className={classes.chessField}
-                          onWheel={e => e.deltaY < 0 ? notation.toPrev() : notation.toNext()}>
-                      <ChessBoard
-                        width={matchesLG ? "544px" : matchesOnlyXS ? "100vmin" : matchesMD ? "448px" : "384px"}
-                        height={matchesLG ? "544px" : matchesOnlyXS ? "100vmin" : matchesMD ? "448px" : "384px"}
-                        orientation={notation.boardOrientation}
-                        viewOnly={false}
-                        turnColor={notation.turnColor()}
-                        movable={notation.calcMovable}
-                        lastMove={notation.lastMove}
-                        fen={notation.currentNode.fen}
-                        check={notation.inCheck}
-                        style={{margin: "auto"}}
-                        coordinates={false}
-                        onMove={notation.onMove}
-                      />
-                   </Paper>
+           {/*{matchesLG && <Grid item container direction={"column"} lg>*/}
 
-                   {!matchesLG && <Grid item>
-                      <Grid container justify={"center"}>
-                         <NavButtons
-                           toFirst={notation.toFirst}
-                           toLast={notation.toLast}
-                           toNext={notation.toNext}
-                           toPrev={notation.toPrev}
-                           onFlip={notation.flipBoard}
-                           onSearchClick={() => setShowSearch(!showSearch)}
-                           onBookClick={() => setShowBook(!showBook)}
-                           onChaptersClick={() => setShowChapters(!showChapters)}
-                           showBook={showBook}
-                           showSearch={showSearch}
-                           showChapters={showChapters && slug}
-                           currentDB={chess.currentDB}
-                           showChaptersButton={slug}
-                         />
-                      </Grid>
-                   </Grid>}
+           {/*   {!slug && <Grid item xs style={{maxHeight: "17vh"}}>*/}
+           {/*      <GameInfo data={notation.gameHeaders}/>*/}
+           {/*   </Grid>}*/}
+           {/*   <Grid item xs sm md lg style={{height: '73vh', overflowY: "auto"}}>*/}
+           {/*      {!slug && <img src={Colobki} alt={''} width={"100%"} height={"auto"}/>}*/}
+           {/*   </Grid>*/}
+           {/*</Grid>}*/}
 
-                   {showSearch &&
-                   <Grid item>
-                      <Fade in={showSearch}>
-                         <GamesSearch
-                           name={name}
-                           color={color !== 'w'}
-                           onSubmit={() => handleSearchSubmit(name, color)}
-                           onChangeColor={setColor}
-                           onChangeName={setName}
-                         /></Fade>
-                   </Grid>}
-                </Grid>
-             </Grid>
-             {matchesOnlyXS && !slug && <Grid item xs={12}>
-                <Grid item><GameInfo data={notation.gameHeaders}/></Grid>
-             </Grid>}
-             <Grid container direction={"column"} sm md lg item style={{
-                height: "92vh",
-                padding: matchesSM ? "8px" : 0,
-                maxHeight: matchesOnlyXS ? "calc(20vh + 100px)" : "none",
-                minHeight: matchesOnlyXS ? "calc(20vh + 100px)" : "none",
 
-             }}>
-                {!showChapters || matchesLG ? <>{(matchesSM || !showBook) && <Grid sm md item style={{
-                   overflow: "auto",
-                }}>
-                   <Notation
-                     notation={notation.rootLine}
-                     currentNode={notation.currentNode}
-                     jumpTo={notation.jumpToMove}
-                     promoteLine={notation.promoteLine}
-                     deleteRemaining={notation.deleteRemaining}
-                     deleteLine={notation.deleteLine}
-                   />
-                </Grid>}
+           <Grid item container justify={"center"}
+                 style={{padding: matchesSM ? "8px" : 0}}
+                 sm md lg>
+              <Grid item>
 
-                   {showBook && <Grid item sm md style={{
-                      overflowY: "auto",
-                   }}>
-                      <Fade in={showBook}>
-                         <ExplorerBox
-                           explorerData={chess.explorerData.moves}
-                           onMove={notation.makeSanMove}
-                           loading={chess.inProgress}
-                           games={chess.explorerData.games}
-                           setShowBook={setShowBook}
-                           currentDB={chess.currentDB}
-                           changeDB={chess.setDatabase}
-                         />
-                      </Fade>
-                   </Grid>}
-                </> : <Grid item style={{overflow: "auto",}}>
-                   <StudyChapters
-                     currentOpening={openings.currentOpening}
-                     currentChapter={openings.currentChapter}
-                   /></Grid>}
+                 {matchesSM && !matchesLG && <Grid item style={{maxHeight: "17vh"}}>
+                    <GameInfo data={notation.gameHeaders}/>
+                 </Grid>}
 
-                {matchesLG && <Grid item>
-                   <Grid container justify={"center"}>
-                      <NavButtons
-                        toFirst={notation.toFirst}
-                        toLast={notation.toLast}
-                        toNext={notation.toNext}
-                        toPrev={notation.toPrev}
-                        onFlip={notation.flipBoard}
-                        onSearchClick={() => setShowSearch(!showSearch)}
-                        onBookClick={() => setShowBook(!showBook)}
-                        onChaptersClick={() => setShowChapters(!showChapters)}
-                        showBook={showBook}
-                        showSearch={showSearch}
-                        showChapters={showChapters && slug}
-                        currentDB={chess.currentDB}
-                        showChaptersButton={slug}
-                      />
-                   </Grid>
-                </Grid>}
-             </Grid>
-          </Grid>}
+                 <ChessBoard/>
 
-        <Dialog open={notation.showPieceSelectMenu} onClose={() => {
-           notation.showPieceSelectMenu = false;
-           notation.pendingMove = null;
-        }}>
-           {['q', 'n', 'r', 'b'].map(piece =>
-             <DialogActions key={piece}>
-                <Button disableRipple onClick={() => notation.promotion(piece)}>
-                   <img src={pieceImages[notation.turnColor()][piece]} alt={''}/>
-                </Button>
-             </DialogActions>
-           )}
-        </Dialog>
-     </>
+                 {!matchesLG && <Grid item>
+                    <Grid container justify={"center"}>
+                       <NavButtons
+                         toFirst={notation.toFirst}
+                         toLast={notation.toLast}
+                         toNext={notation.toNext}
+                         toPrev={notation.toPrev}
+                         onFlip={notation.flipBoard}
+                         onSearchClick={toggleSearch}
+                         onBookClick={toggleBook}
+                         onChaptersClick={toggleChapters}
+                         showBook={showBook}
+                         showSearch={showSearch}
+                         showChapters={showChapters && slug}
+                         currentDB={chess.currentDB}
+                         showChaptersButton={slug}
+                       />
+                    </Grid>
+                 </Grid>}
+
+                 {showSearch &&
+                 <Grid item>
+                    <Fade in={showSearch}>
+                       <GamesSearch
+                         name={name}
+                         color={color !== 'w'}
+                         onSubmit={handleSearchSubmit}
+                         onChangeColor={setColor}
+                         onChangeName={setName}
+                       /></Fade>
+                 </Grid>}
+              </Grid>
+
+           </Grid>
+
+           {matchesOnlyXS && !slug && <Grid item xs={12}>
+              <Grid item><GameInfo data={notation.gameHeaders}/></Grid>
+           </Grid>}
+
+           <Grid container direction={"column"} sm md lg item>
+              <>{(matchesSM || !showBook) && <Grid sm md item>
+                 <Notation
+                   notation={notation.rootLine}
+                   currentNode={notation.currentNode}
+                   jumpTo={notation.jumpToMove}
+                   promoteLine={notation.promoteLine}
+                   deleteRemaining={notation.deleteRemaining}
+                   deleteLine={notation.deleteLine}
+                 />
+              </Grid>}
+
+                 {showBook && <Grid item sm md>
+                    <Fade in={showBook}>
+                       <ExplorerBox
+                         explorerData={chess.explorerData.moves}
+                         onMove={notation.makeSanMove}
+                         loading={chess.inProgress}
+                         games={chess.explorerData.games}
+                         setShowBook={setShowBook}
+                         currentDB={chess.currentDB}
+                         changeDB={chess.setDatabase}
+                       />
+                    </Fade>
+                 </Grid>}
+              </>
+
+              {matchesLG && <Grid item>
+                 <Grid container justify={"center"}>
+                    <NavButtons
+                      toFirst={notation.toFirst}
+                      toLast={notation.toLast}
+                      toNext={notation.toNext}
+                      toPrev={notation.toPrev}
+                      onFlip={notation.flipBoard}
+                      onSearchClick={toggleSearch}
+                      onBookClick={toggleBook}
+                      onChaptersClick={toggleChapters}
+                      showBook={showBook}
+                      showSearch={showSearch}
+                      showChapters={showChapters && slug}
+                      currentDB={chess.currentDB}
+                      showChaptersButton={slug}
+                    />
+                 </Grid>
+              </Grid>}
+           </Grid>
+        </Grid>
+
+     </div>
    )
 }
 
